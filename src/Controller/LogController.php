71358b2858fff4e56f7494c65612793bbdf5fecb
allanlepinay <allanlepinayjp@gmail.com>
2023-07-06 19:57:47 +0900

    homepage access configuration

---
+++ src/Controller/LogController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class LogController extends AbstractController
+{
+    #[Route('/log', name: 'app_log')]
+    public function index(): Response
+    {
+        return $this->render('log/index.html.twig', [
+            'controller_name' => 'LogController',
+            'app_user' => '',
+        ]);
+    }
+}
